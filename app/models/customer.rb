36e4902cd5117e41069d13c8241fbbae407d08c0
jsarmiento4rrc <jsarmiento4@rrc.ca>
2026-03-23 14:06:06 -0500

    Generate models, associations, and validations for ampm Pro Audio ERD

---
+++ app/models/customer.rb
@@ -0,0 +1,6 @@
+class Customer < ApplicationRecord
+  has_many :orders
+  
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
+end
---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :order_items
+  has_many :products, through: :order_items
+  
+  validates :status, presence: true
+end
